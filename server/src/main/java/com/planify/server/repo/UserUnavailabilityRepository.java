d688516331ea20d990d5d0cbb711bc6caf167068
Sophie Palmade <sophie.palmade@imt-atlantique.net>
2024-11-19 14:14:18 +0100

    Création repositories part 3

--- server/src/main/java/com/planify/server/repo/UserUnavailabilityRepository.java
+++ server/src/main/java/com/planify/server/repo/UserUnavailabilityRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.planify.server.models.Slot;
 import com.planify.server.models.User;
 import com.planify.server.models.UserUnavailability;
 import com.planify.server.models.UserUnavailability.UserUnavailabilityId;
 
+@Repository
 public interface UserUnavailabilityRepository extends JpaRepository<UserUnavailability, UserUnavailabilityId> {
 
     List<UserUnavailability> findAll();
--- server/src/main/java/com/planify/server/repo/WeekRepository.java
+++ server/src/main/java/com/planify/server/repo/WeekRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.planify.server.models.Week;
 
+@Repository
 public interface WeekRepository extends JpaRepository<Week, Long> {
 
     List<Week> findAll();
