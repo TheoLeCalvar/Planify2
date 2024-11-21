ed5467af8c42c1cdc4061b272e3e7fbaa297a4b1
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-11-21 16:43:30 +0100

    feat: Add helper components for confirmation (dialog and button)

---
+++ client/src/components/ConfirmationButton.jsx
@@ -0,0 +1,60 @@
+// src/components/ConfirmationButton.js
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Button } from "@mui/material";
+import ConfirmationDialog from "./ConfirmationDialog";
+
+const ConfirmationButton = ({
+    buttonComponent,
+    variant,
+    buttonText = "Effectuer une action",
+    buttonColor = "primary",
+    dialogTitle = "Confirmation",
+    dialogMessage = "Êtes-vous sûr de vouloir effectuer cette action ?",
+    confirmText = "Confirmer",
+    cancelText = "Annuler",
+    onConfirm = () => {},
+}) => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <>
+            {/* Bouton déclencheur */}
+            {buttonComponent || (
+                <Button
+                    variant={variant ?? "contained"}
+                    color={buttonColor}
+                    onClick={handleOpen}
+                >
+                    {buttonText}
+                </Button>
+            )}
+
+            {/* Popup de confirmation */}
+            <ConfirmationDialog
+                cancelText={cancelText}
+                confirmText={confirmText}
+                dialogMessage={dialogMessage}
+                dialogTitle={dialogTitle}
+                onConfirm={onConfirm}
+                handleClose={handleClose}
+                open={open}
+            />
+        </>
+    );
+};
+
+ConfirmationButton.propTypes = {
+    buttonText: PropTypes.string,
+    buttonColor: PropTypes.string,
+    dialogTitle: PropTypes.string,
+    dialogMessage: PropTypes.string,
+    confirmText: PropTypes.string,
+    cancelText: PropTypes.string,
+    onConfirm: PropTypes.func,
+};
+
+export default ConfirmationButton;
---
+++ client/src/components/ConfirmationDialog.jsx
@@ -0,0 +1,71 @@
+// src/components/ConfirmationButton.js
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@mui/material";
+
+const ConfirmationDialog = ({
+    dialogTitle = "Confirmation",
+    dialogMessage = "Êtes-vous sûr de vouloir effectuer cette action ?",
+    confirmText = "Confirmer",
+    cancelText = "Annuler",
+    onConfirm = () => {},
+    onCancel = () => {},
+    open,
+    handleClose
+}) => {
+    const handleConfirm = () => {
+        onConfirm();
+        handleClose();
+    };
+
+    const handleCancel = () => {
+        onCancel();
+        handleClose();
+    }
+
+    return (
+        <>
+            {/* Popup de confirmation */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="confirmation-dialog-title"
+                aria-describedby="confirmation-dialog-description"
+            >
+                <DialogTitle id="confirmation-dialog-title">
+                    {dialogTitle}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="confirmation-dialog-description">
+                        {dialogMessage}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancel} color="secondary">
+                        {cancelText}
+                    </Button>
+                    <Button onClick={handleConfirm} variant="contained" color="primary" autoFocus>
+                        {confirmText}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+};
+
+ConfirmationDialog.propTypes = {
+    dialogTitle: PropTypes.string,
+    dialogMessage: PropTypes.string,
+    confirmText: PropTypes.string,
+    cancelText: PropTypes.string,
+    onConfirm: PropTypes.func,
+};
+
+export default ConfirmationDialog;
