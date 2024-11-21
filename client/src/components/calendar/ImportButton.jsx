db1a99900f6b172de23d5d9552465bba95875305
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-11-21 16:44:10 +0100

    feat(calendar): Add confirmation before importing calendar

--- client/src/components/calendar/ImportButton.jsx
+++ client/src/components/calendar/ImportButton.jsx
@@ -1,15 +1,21 @@
-import { Button } from "@mui/material";
 import importCalendar from '../../helper/importCalendar'
 import { constants } from '../../contants'
 import { useContext } from 'react'
 import { CalendarContext } from '../../context/CalendarContext'
+import ConfirmationButton from "../ConfirmationButton";
 
 export default function ImportButton(){
 
     const { eventService, genericEventService } = useContext(CalendarContext)
 
     return(
-        <Button variant="text" onClick={() => {
+        <ConfirmationButton 
+          variant="text" 
+          buttonText="Importer"
+          dialogTitle="Avertissement"
+          dialogMessage="Importer un calendrier écrasera les données actuelles."
+          confirmText="Poursuivre l'importation"
+          onConfirm={() => {
             importCalendar(constants.CALENDAR.TYPES.AVAILABILITY)
             .then(({events, genericEvents}) => {
                 eventService.set(events)
@@ -18,6 +24,6 @@ export default function ImportButton(){
             .catch((error) => {
               console.error(error)
             })
-          }}>Importer</Button>
+          }}/>
     )
 }
