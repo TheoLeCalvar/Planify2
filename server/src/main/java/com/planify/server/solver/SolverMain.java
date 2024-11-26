62e0bc4107e0c86de86de46f3d72ada11d49de97
EragonKing2020 <Eragonking2020@gmail.com>
2024-11-26 11:45:42 +0100

    Large outline of the solver part

--- server/src/main/java/com/planify/server/solver/SolverMain.java
+++ server/src/main/java/com/planify/server/solver/SolverMain.java
@@ -1,13 +1,20 @@
 package com.planify.server.solver;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+
 import com.planify.server.models.Calendar;
 import com.planify.server.models.TAF;
 
 public class SolverMain {
 	public static void generateTaf(TAF taf, Calendar cal) {
-		
+		Model model = new Model();
+		Solver solver = model.getSolver();
+		taf.initialiseVars(model);
+		setConstraints(model, taf);
+		solver.setSearch(taf.getStrategy());
+		solver.findOptimalSolution(obj, false, null);
 	}
-	
 	
 	
 	
