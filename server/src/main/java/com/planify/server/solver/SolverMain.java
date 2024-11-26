d8e94de4496c87da7619c22e99e22fe6793e9d74
EragonKing2020 <Eragonking2020@gmail.com>
2024-11-26 11:37:35 +0100

    Creation of the branch solver

---
+++ server/src/main/java/com/planify/server/solver/SolverMain.java
@@ -0,0 +1,20 @@
+package com.planify.server.solver;
+
+import com.planify.server.models.Calendar;
+import com.planify.server.models.TAF;
+
+public class SolverMain {
+	public static void generateTaf(TAF taf, Calendar cal) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
