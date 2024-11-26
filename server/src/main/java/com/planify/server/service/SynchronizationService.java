ded9288114ef07b96585bc4eb05b7fa7b5d25ce8
Sophie Palmade <sophie.palmade@imt-atlantique.net>
2024-11-26 10:27:28 +0100

    Ajout service part 6

--- server/src/main/java/com/planify/server/service/SynchronizationService.java
+++ server/src/main/java/com/planify/server/service/SynchronizationService.java
@@ -1,12 +1,14 @@
 package com.planify.server.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.planify.server.models.Lesson;
 import com.planify.server.models.Synchronization;
+import com.planify.server.models.Synchronization.SynchronizationId;
 import com.planify.server.repo.SynchronizationRepository;
 
 @Service
@@ -15,18 +17,30 @@ public class SynchronizationService {
     @Autowired
     private SynchronizationRepository synchronizationRepository;
 
-    List<Synchronization> findByLesson(Lesson lesson) {
-        List<Synchronization> listSync1 = synchronizationRepository.findByLesson1(lesson);
-        List<Synchronization> listSync2 = synchronizationRepository.findByLesson2(lesson);
-        listSync1.addAll(listSync2);
-        return listSync1;
-    }
-
-    Synchronization save(Lesson lesson1, Lesson lesson2) {
+    public Synchronization addSynchronization(Lesson lesson1, Lesson lesson2) {
         Synchronization synchronization = new Synchronization(lesson1, lesson2);
 
         synchronizationRepository.save(synchronization);
         return synchronization;
+    }
+
+    public boolean deleteSynchronization(SynchronizationId id) {
+        if (synchronizationRepository.existsById(id)) {
+            synchronizationRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<Synchronization> findById(SynchronizationId id) {
+        return synchronizationRepository.findById(id);
+    }
+
+    public List<Synchronization> findByLesson(Lesson lesson) {
+        List<Synchronization> listSync1 = synchronizationRepository.findByLesson1(lesson);
+        List<Synchronization> listSync2 = synchronizationRepository.findByLesson2(lesson);
+        listSync1.addAll(listSync2);
+        return listSync1;
     }
 
 }
--- server/src/main/java/com/planify/server/service/TAFService.java
+++ server/src/main/java/com/planify/server/service/TAFService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.planify.server.models.Calendar;
 import com.planify.server.models.TAF;
 import com.planify.server.models.TAFManager;
 import com.planify.server.models.UE;
@@ -38,6 +39,28 @@ public class TAFService {
         if (tafRepository.existsById(id)) {
             TAF taf = tafRepository.findById(id).get();
 
+            // Delete the UEs of this TAF
+            List<UE> listUes = taf.getUes();
+            for (UE ue : listUes) {
+                ueRepository.delete(ue);
+            }
+
+            // Delete the calendars associated to this TAF
+            List<Calendar> listCalendars = taf.getCalendars();
+            for (Calendar c : listCalendars) {
+                calendarRepository.delete(c);
+            }
+
+            // Delete the TAFManager of this TAF
+            List<TAFManager> listManagers = taf.getTafManagers();
+            for (TAFManager m : listManagers) {
+                tafManagerRepository.delete(m);
+            }
+
+            // Delete the taf in the TAF's table
+            tafRepository.delete(taf);
+
+            return true;
         }
 
         return false;
