5d4907fe3d5d7d82b9a2fb9b500fcda08c324c0e
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-12-10 14:06:54 +0100

    style(courses): Improve UI/UX by displaying courses buttons only when cursor is over block/course

--- client/src/components/courses/Block.jsx
+++ client/src/components/courses/Block.jsx
@@ -14,6 +14,7 @@ import { Draggable, Droppable } from "@hello-pangea/dnd";
 import Course from "./Course";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
 import ConfirmationButton from "../ConfirmationButton";
+import { useState } from "react";
 
 export default function Block({
     block,
@@ -26,104 +27,114 @@ export default function Block({
     onDeleteCourse,
     onDuplicateCourse,
 }) {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <Paper elevation={2} sx={{ padding: 2 }}>
-            <Stack
-                direction="row"
-                justifyContent="space-between"
-                alignItems="center"
-            >
-                <Typography variant="h6">{block.title}</Typography>
-                { dependencies.length > 0 ? (
-                    <Typography variant="body2" color="textSecondary">
-                        Précédence: {dependencies.join(", ")}
-                    </Typography>
-                ) :
-                    <Typography variant="body2" color="textSecondary">
-                        Pas de précédence
-                    </Typography>
-                }
-                <Box>
-                    <Button
-                        startIcon={<AddIcon />}
-                        variant="text"
-                        onClick={() => onAddCourse(block.id)}
-                        sx={{ marginRight: 3 }}
-                    >
-                        Ajouter un cours
-                    </Button>
-                    <Tooltip title="Editer">
-                        <IconButton onClick={onEdit} color="primary">
-                            <EditIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <ConfirmationButton
-                        buttonComponent={
-                            <IconButton color="secondary">
-                                <DeleteIcon />
+        <Box
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <Paper elevation={2} sx={{ padding: 2 }}>
+                <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Typography variant="h6">{block.title}</Typography>
+                    {dependencies.length > 0 ? (
+                        <Typography variant="body2" color="textSecondary">
+                            Précédence: {dependencies.join(", ")}
+                        </Typography>
+                    ) : (
+                        <Typography variant="body2" color="textSecondary">
+                            Pas de précédence
+                        </Typography>
+                    )}
+                    <Box visibility={isHovered ? "visible" : "hidden"}>
+                        <Button
+                            startIcon={<AddIcon />}
+                            variant="text"
+                            onClick={() => onAddCourse(block.id)}
+                            sx={{ marginRight: 3 }}
+                        >
+                            Ajouter un cours
+                        </Button>
+                        <Tooltip title="Editer">
+                            <IconButton onClick={onEdit} color="primary">
+                                <EditIcon />
                             </IconButton>
-                        }
-                        onConfirm={onDelete}
-                        tooltip={"Supprimer"}
-                        dialogTitle="Supprimer le bloc ?"
-                        dialogMessage={`Êtes-vous sûr de vouloir supprimer le bloc '${block.title}' ?`}
-                    />
-                    <Tooltip title="Dupliquer">
-                        <IconButton onClick={onDuplicate} color="secondary">
-                            <FileCopyIcon />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-            </Stack>
-            <Typography variant="body2" color="textSecondary" gutterBottom>
-                {block.description || "Aucune description fournie"}
-            </Typography>
-            <Droppable droppableId={`courses-${block.id}`} type="course">
-                {(provided) => (
-                    <Box
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                        sx={{
-                            marginTop: 2,
-                            padding: 1,
-                            border: "1px dashed gray",
-                        }}
-                    >
-                        {block.courses.map((course, index) => (
-                            <Draggable
-                                key={course.id}
-                                draggableId={`course-${course.id}`}
-                                index={index}
-                            >
-                                {(provided) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                    >
-                                        <Course
-                                            course={course}
-                                            onEdit={() =>
-                                                onEditCourse(block.id, course)
-                                            }
-                                            onDelete={() =>
-                                                onDeleteCourse(course.id)
-                                            }
-                                            onDuplicate={() =>
-                                                onDuplicateCourse(
-                                                    block.id,
-                                                    course
-                                                )
-                                            }
-                                        />
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
+                        </Tooltip>
+                        <ConfirmationButton
+                            buttonComponent={
+                                <IconButton color="secondary">
+                                    <DeleteIcon />
+                                </IconButton>
+                            }
+                            onConfirm={onDelete}
+                            tooltip={"Supprimer"}
+                            dialogTitle="Supprimer le bloc ?"
+                            dialogMessage={`Êtes-vous sûr de vouloir supprimer le bloc '${block.title}' ?`}
+                        />
+                        <Tooltip title="Dupliquer">
+                            <IconButton onClick={onDuplicate} color="secondary">
+                                <FileCopyIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Box>
-                )}
-            </Droppable>
-        </Paper>
+                </Stack>
+                <Typography variant="body2" color="textSecondary" gutterBottom>
+                    {block.description || "Aucune description fournie"}
+                </Typography>
+                <Droppable droppableId={`courses-${block.id}`} type="course">
+                    {(provided) => (
+                        <Box
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                            sx={{
+                                marginTop: 2,
+                                padding: 1,
+                                border: "1px dashed gray",
+                            }}
+                        >
+                            {block.courses.map((course, index) => (
+                                <Draggable
+                                    key={course.id}
+                                    draggableId={`course-${course.id}`}
+                                    index={index}
+                                >
+                                    {(provided) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                        >
+                                            <Course
+                                                course={course}
+                                                onEdit={() =>
+                                                    onEditCourse(
+                                                        block.id,
+                                                        course
+                                                    )
+                                                }
+                                                onDelete={() =>
+                                                    onDeleteCourse(course.id)
+                                                }
+                                                onDuplicate={() =>
+                                                    onDuplicateCourse(
+                                                        block.id,
+                                                        course
+                                                    )
+                                                }
+                                            />
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </Box>
+                    )}
+                </Droppable>
+            </Paper>
+        </Box>
     );
 }
--- client/src/components/courses/Course.jsx
+++ client/src/components/courses/Course.jsx
@@ -5,46 +5,57 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
 import ConfirmationButton from "../ConfirmationButton";
+import { useState } from "react";
 
 export default function Course({ course, onEdit, onDelete, onDuplicate }) {
+    const [isHovered, setIsHovered] = useState(false);
     return (
-        <Paper
-            elevation={1}
-            sx={{
-                padding: 1,
-                marginBottom: 1,
-                backgroundColor: "#f9f9f9",
-            }}
+        <Box
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         >
-            <Stack direction="row" justifyContent="space-between" alignItems="center">
-                <Typography variant="subtitle1">{course.title}</Typography>
-                <Box>
-                    <Tooltip title="Editer">
-                        <IconButton onClick={onEdit} color="primary">
-                            <EditIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <ConfirmationButton
-                        buttonComponent={
-                            <IconButton color="secondary">
-                                <DeleteIcon />
+            <Paper
+                elevation={1}
+                sx={{
+                    padding: 1,
+                    marginBottom: 1,
+                    backgroundColor: "#f9f9f9",
+                }}
+            >
+                <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Typography variant="subtitle1">{course.title}</Typography>
+                    <Box visibility={isHovered ? "visible" : "hidden"}>
+                        <Tooltip title="Editer">
+                            <IconButton onClick={onEdit} color="primary">
+                                <EditIcon />
                             </IconButton>
-                        }
-                        onConfirm={onDelete}
-                        dialogTitle="Supprimer le cours ?"
-                        tooltip={"Supprimer"}
-                        dialogMessage={`Êtes-vous sûr de vouloir supprimer le cours '${course.title}' ?`}
-                    />
-                    <Tooltip title="Dupliquer">
-                        <IconButton onClick={onDuplicate} color="secondary">
-                            <FileCopyIcon />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-            </Stack>
-            <Typography variant="body2" color="textSecondary">
-                {course.description || "Aucune description fournie"}
-            </Typography>
-        </Paper>
+                        </Tooltip>
+                        <ConfirmationButton
+                            buttonComponent={
+                                <IconButton color="secondary">
+                                    <DeleteIcon />
+                                </IconButton>
+                            }
+                            onConfirm={onDelete}
+                            dialogTitle="Supprimer le cours ?"
+                            tooltip={"Supprimer"}
+                            dialogMessage={`Êtes-vous sûr de vouloir supprimer le cours '${course.title}' ?`}
+                        />
+                        <Tooltip title="Dupliquer">
+                            <IconButton onClick={onDuplicate} color="secondary">
+                                <FileCopyIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                </Stack>
+                <Typography variant="body2" color="textSecondary">
+                    {course.description || "Aucune description fournie"}
+                </Typography>
+            </Paper>
+        </Box>
     );
 }
