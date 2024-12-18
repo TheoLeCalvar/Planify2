61c9783265b7c159f0b636a58c9b62dbb78a4e9c
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-12-18 15:56:28 +0100

    change(server): Fix CORS issues

--- server/src/main/java/com/planify/server/config/WebConfig.java
+++ server/src/main/java/com/planify/server/config/WebConfig.java
@@ -13,9 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("🚀 CORS configuration is being loaded!");
         registry.addMapping("/**") // Toutes les routes de l'application
-                .allowedOrigins("http://localhost:5173") // Origines autorisées
+                .allowedOrigins("*") // Origines autorisées
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
-                .allowedHeaders("*") // Tous les headers autorisés
-                .allowCredentials(true); // Autoriser les cookies ou identifiants
+                .allowedHeaders("*"); // Tous les headers autorisés
+                //.allowCredentials(true); // Autoriser les cookies ou identifiants
     }
 }
