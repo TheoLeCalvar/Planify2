925b30d11dd8df49b1e23ea1e1ecc868fc78e039
Sophie Palmade <sophie.palmade@imt-atlantique.net>
2025-01-23 19:59:44 +0100

    Fixit

--- server/src/main/java/com/planify/server/models/ScheduledLesson.java
+++ server/src/main/java/com/planify/server/models/ScheduledLesson.java
@@ -13,11 +13,11 @@ public class ScheduledLesson {
 
     private Long id;
 
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime start; // date of the start of the lesson
 
     @Column(name = "\"end\"")
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime end; // date of the end of the lesson
 
     private String UE; // name of the UE
