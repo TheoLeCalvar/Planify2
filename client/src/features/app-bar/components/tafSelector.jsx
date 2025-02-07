a3b52ad6b13e3cb0eb939f169eaef30a1c0f4190
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-02-07 15:30:02 +0100

    refactor: refactored app bar into a custom component and added a custom hook for taf selection

--- client/src/features/app-bar/components/tafSelector.jsx
+++ client/src/features/app-bar/components/tafSelector.jsx
@@ -9,16 +9,22 @@ import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import layout from "../../../config/locale.json";
 import styles from "./tafSelector.styles";
 
-// TAF Selector Component
 const TAFSelector = ({ selectedOption, onChange, tafs }) => {
+  // Wrap the onChange to extract the value and then forward it.
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
+
   return (
     <Box sx={styles.TAFSelectorBox}>
       <FormControl variant="standard" sx={styles.formControl}>
-        <InputLabel htmlFor="dropdown-selector"></InputLabel>
+        <InputLabel htmlFor="dropdown-selector">
+          {layout.layout.appBar.TAFSelector}
+        </InputLabel>
         <Select
           id="dropdown-selector"
           value={selectedOption}
-          onChange={onChange}
+          onChange={handleChange}
           label={layout.layout.appBar.TAFSelector}
         >
           {tafs.map(({ id, name }) => (
