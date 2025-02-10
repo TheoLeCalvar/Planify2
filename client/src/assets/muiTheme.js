ec696aab690313c6da48848a1f876d72e24cc557
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-02-10 16:33:26 +0100

    style: added default IMT style

--- client/src/assets/muiTheme.js
+++ client/src/assets/muiTheme.js
@@ -1,6 +1,15 @@
 import { createTheme } from "@mui/material/styles";
 
 const muiTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0C2E40",
+    },
+    secondary: {
+      main: "#22828f",
+    },
+  },
 });
 
 export default muiTheme;
